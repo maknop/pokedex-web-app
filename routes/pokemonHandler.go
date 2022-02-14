37b438b63ca9159c4cfe300e83cec777b662f15e
Matthew Knop <knopmatthew93@gmail.com>
2022-02-13 21:43:50 -0700

    Created route handlers for pokemon.

---
+++ routes/pokemonHandler.go
@@ -0,0 +1,40 @@
+package pokemonHandler
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getAllPokemon(router *gin.Engine) {
+	router.GET("/pokemon", func(c *gin.Context) {
+		resp, err := http.Get("https://pokeapi.co/api/v2/pokemon")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		all_pokemon, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(all_pokemon))
+
+		defer resp.Body.Close()
+	})
+}
+
+func getPokemonById(router *gin.Engine) {
+	router.GET("/pokemon/:id", func(c *gin.Context) {
+		pokemon_id := c.Param("id")
+		resp, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", pokemon_id))
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		all_pokemon, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(all_pokemon))
+
+		defer resp.Body.Close()
+	})
+}
--- server.go
+++ server.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"pokedex-api/routes/pokemonHandler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,42 +11,13 @@ type Pokemon struct {
 	Name string `json:"name"`
 }
 
-var pokemon = []Pokemon{}
-
-func getPokemon(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, pokemon)
-}
-
 func main() {
-	router := gin.New()
+	router := gin.Default()
 
 	router.Use(gin.Logger())
 
-	router.GET("/pokemon", func(c *gin.Context) {
-		resp, err := http.Get("https://pokeapi.co/api/v2/pokemon")
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		all_pokemon, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println(string(all_pokemon))
-
-		defer resp.Body.Close()
-	})
-
-	router.GET("/pokemon/:id", func(c *gin.Context) {
-		pokemon_id := c.Param("id")
-		resp, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", pokemon_id))
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		all_pokemon, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println(string(all_pokemon))
-
-		defer resp.Body.Close()
-	})
+	pokemonHandler.Routes(router)
+	pokemonHandler.Routes(router)
 
 	router.Run("localhost:8080")
 }
