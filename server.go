f19468639f60442b500ed49f82c702b68ef82b4a
Matt Knop <knopmatthew93@gmail.com>
2022-02-15 20:40:52 -0700

    Updated logs.

--- server.go
+++ server.go
@@ -7,11 +7,6 @@ import (
 	pokemon "pokedex-api/routes/pokemon"
 )
 
-type Pokemon struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
-
 func loggingOutput() {
 	logFile, _ := os.Create("logs/api_requests.log")
 
@@ -22,6 +17,7 @@ func loggingOutput() {
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/css", "../css")
 
 	// Writing logs to file
 	loggingOutput()
@@ -31,7 +27,6 @@ func main() {
 	// Routes
 	pokemon.GetAllPokemon(router)
 	pokemon.GetPokemonById(router)
-	pokemon.GetPokemonByName(router)
 
 	router.Run("localhost:8080")
 }
