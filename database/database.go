dc0aacb58b0ff476fb6ba33ab3969fdfd620d7e9
Matt Knop <knopmatthew93@gmail.com>
2024-03-07 18:49:00 -0700

    formatting, database work

--- database/database.go
+++ database/database.go
@@ -2,15 +2,17 @@ package database
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	models "github.com/maknop/pokedex-web-app/models"
+	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+// Connect - Establish connection to postgres database
 func Connect() *gorm.DB {
+	log.Info("setting database environment variables")
 	postgresUser := os.Getenv("POSTGRES_USER")
 	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
 	postgresDatabase := os.Getenv("POSTGRES_DB")
@@ -18,6 +20,7 @@ func Connect() *gorm.DB {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", postgresHost, postgresUser, postgresPassword, postgresDatabase)
 
+	log.Info("setting up connection to postgresql database")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"time"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 
 	data "github.com/maknop/pokedex-web-app/data"
-	pokemon "github.com/maknop/pokedex-web-app/routes"
 	db "github.com/maknop/pokedex-web-app/database"
+	pokemon "github.com/maknop/pokedex-web-app/routes"
 )
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
+}
 
 func main() {
 	router := gin.Default()
@@ -27,6 +39,4 @@ func main() {
 	pokemon.PokemonById(router, allPokemon)
 
 	router.Run("localhost:8080")
-
-	time.Sleep(5 * time.Second)
 }
