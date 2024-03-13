773f5f1ff8d36094b6e8e0ad163c6e65a451f844
Matt Knop <knopmatthew93@gmail.com>
2024-03-12 21:21:42 -0600

    fixed database connection issue (temp fix)

--- database/database.go
+++ database/database.go
@@ -1,49 +1,35 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
-	"github.com/maknop/pokedex-web-app/models"
+	_ "github.com/lib/pq"
+
 	log "github.com/sirupsen/logrus"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
-type Dbinstance struct {
-	Db *gorm.DB
-}
+var db *sql.DB
 
-var DB Dbinstance
-
-// Connect - Establish connection to postgres database
-func Connect() {
+func EstablishConnection() {
 	log.Info("setting database environment variables")
 	postgresUser := os.Getenv("POSTGRES_USER")
 	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
 	postgresDatabase := os.Getenv("POSTGRES_DB")
 	postgresHost := os.Getenv("POSTGRES_HOST")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
-		postgresHost, postgresUser, postgresPassword, postgresDatabase)
-
-	log.Info("setting up connection to postgresql database")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+	db, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s sslmode=disable password=%s host=%s", postgresUser, postgresDatabase, postgresPassword, postgresHost))
 	if err != nil {
-		log.Fatal("Failed to connect to database. \n", err)
-		os.Exit(2)
-	}
-	log.Println("connected")
-	db.Logger = logger.Default.LogMode(logger.Info)
-
-	log.Println("running migrations")
-	db.AutoMigrate(&models.Pokemon{})
-
-	DB = Dbinstance{
-		Db: db,
+		log.Fatalf(fmt.Sprintf("connection to postgres container failed: %s", err))
 	}
 
+	defer db.Close()
+
+	// Test the connection to the database
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Successfully Connected")
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
@@ -14,13 +14,6 @@ import (
 func init() {
 	// Log as JSON instead of the default ASCII formatter.
 	log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	log.SetLevel(log.WarnLevel)
 }
 
 func main() {
@@ -28,7 +21,8 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl")
 	router.Static("/styles", "./styles")
 
-	db.Connect()
+	time.Sleep(10 * time.Second)
+	go db.EstablishConnection()
 
 	allPokemon := data.GetPokemonData()
 
