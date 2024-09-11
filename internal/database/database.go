e24b242ab36273d92bcf4c5187e75e22b8dc3838
Matthew Knop <knopmatthew93@gmail.com>
2024-09-10 21:24:32 -0600

    started logic for connections

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,5 +1,20 @@
 package database
 
-func placeholder() {
+import (
+	"database/sql"
+	"fmt"
 
+	_ "github.com/lib/pq"
+)
+
+func Connect() error {
+	connStr := "user=ADMIN dbname=pokemon sslmode=disable"
+	_, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return fmt.Errorf("connection failed: %s\n", err)
+	}
+
+	fmt.Print("connection to postgres database was successful!")
+
+	return nil
 }
