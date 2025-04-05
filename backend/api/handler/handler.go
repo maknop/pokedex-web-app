f2b211dd61f6550cc5faa40ccb506da419910328
Matt Knop <knopmatthew93@gmail.com>
2025-04-05 10:03:31 -0600

    updating project structure, started writing backend

---
+++ backend/api/handler/handler.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Operation struct {
+	Query         string                 `json:"query"`
+	Variables     map[string]interface{} `json:"variables"`
+	OperationName string                 `json:"operationName"`
+}
+
+const URL = "https://beta.pokeapi.co/graphql/v1beta"
+
+var (
+	query = Operation{
+		OperationName: "pokemon_details",
+		Variables: map[string]interface{}{
+			"name": "staryu",
+		},
+		Query: ``,
+	}
+)
+
+func allPokemon(w http.ResponseWriter, req *http.Request) {
+	log.Info(w, "All the Pokemon!\n")
+}
+
+func pokemonDetails(w http.ResponseWriter, req *http.Request) {
+	log.Info("requesting details for pokemon []")
+	body, err := json.Marshal(query)
+	if err != nil {
+		log.Error(err)
+	}
+
+	resp, err := http.Post(URL, "", bytes.NewReader(body))
+	if err != nil {
+		log.Error(err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error(err)
+	}
+	log.Info(string(body))
+}
+
+func main() {
+	log.Info("server running at http://localhost:8080")
+
+	log.Info("serving endpoint at /pokemon")
+	http.HandleFunc("/pokemon", allPokemon)
+
+	log.Info("serving endpoint at /pokemon/:name")
+	http.HandleFunc("/pokemon/:name", pokemonDetails)
+
+	http.ListenAndServe(":8080", nil)
+}
